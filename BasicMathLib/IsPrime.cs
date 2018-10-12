3d5ab03e26e85b44b946482acb4990930c1bd0cd
Shizukanawa <deviousfinest@gmail.com>
2018-10-12 14:37:57 +0200

    Added solution files

---
+++ BasicMathLib/IsPrime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shizukanawa.BasicMathLib
+{
+    public class isPrime
+    {
+        public static bool Check(int checkNumber)
+        {
+            if (checkNumber <= 1) return false;
+            else if (checkNumber <= 3) return true;
+            if (checkNumber % 2 == 0 || checkNumber % 3 == 0) return false;
+
+            for (int i = 5; i * i <= checkNumber; i = i + 6)
+            {
+                if (checkNumber % i == 0 || checkNumber % (i + 2) == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ BasicMathLib/SMath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Shizukanawa.BasicMathLib
+{
+    public class SMath
+    {
+        public static double Add(double number1, double number2) => number1 + number2;
+
+        public static double Subtract(double number1, double number2) => number1 - number2;
+
+        public static double Multiply(double number1, double number2) => number1 * number2;
+
+        public static double Divide(double number1, double number2) => number1 / number2;
+    }
+}
